b66c3e9e806b4ed8768f1411a5372a638a8ef617
Reaper <ahoy@barelyhuman.dev>
2022-04-28 12:43:27 +0530

    test: add usage differences and exec

--- tests/cani.test.ts
+++ tests/cani.test.ts
@@ -2,20 +2,46 @@ import { test } from "uvu";
 import * as assert from "uvu/assert";
 import cani from "../src";
 
-cani.define(
+// use returned set of definition
+const { can } = cani.define(
   "meetWife",
   async (my: { id: number }, wife: { husbandId: number }) => {
     return my.id === wife.husbandId;
   }
 );
 
+// define other stuff somewhere before execution
+
+cani.define(
+  "meetWithGirlfriend",
+  async (my: { id: number; hasWife: true }, girlFriend: { bfId: number }) => {
+    return !my.hasWife && my.id === girlFriend.bfId;
+  }
+);
+
 test("Simple check should return true", async () => {
-  const canDo = await cani.can.meetWife({ id: 1 }, { husbandId: 1 });
+  const canDo = await can.meetWife({ id: 1 }, { husbandId: 1 });
   assert.ok(canDo);
 });
 
 test("Simple check should return false", async () => {
-  const canDo = await cani.can.meetWife({ id: 1 }, { husbandId: 2 });
+  const canDo = await can.meetWife({ id: 1 }, { husbandId: 2 });
+  assert.not.ok(canDo);
+});
+
+test("additional definitions should return true", async () => {
+  const canDo = await can.meetWithGirlfriend(
+    { id: 1, hasWife: false },
+    { bfId: 1 }
+  );
+  assert.ok(canDo);
+});
+
+test("additional definitions should return false", async () => {
+  const canDo = await can.meetWithGirlfriend(
+    { id: 1, hasWife: true },
+    { bfId: 1 }
+  );
   assert.not.ok(canDo);
 });
 
